0099b1080bf7856c9557cbabdbeb54a31a4b40ae
itssoumodip <soumodipddas2005@gmail.com>
2025-05-14 20:37:21 +0530

    layout and page components; enhance InterviewCard with dynamic data and styling

--- interprepai/app/(root)/layout.tsx
+++ interprepai/app/(root)/layout.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import {ReactNode} from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href='/' className='flex items-center gap-2'>
+          <Image src = "/logo.svg" alt="Logo" width={38} height={32} />
+        </Link>
+        <h2 className='text-primary-100'>InterPrep.AI</h2>
+      </nav>
+      {children}
+    </div>
   )
 }
 
