69c9ba10dcc7b167a1a40daffef2dfa21e94c2da
itssoumodip <soumodipddas2005@gmail.com>
2025-05-15 20:08:14 +0530

    feat(AuthForm): add sign-in functionality and handle token retrieval

--- interprepai/components/AuthForm.tsx
+++ interprepai/components/AuthForm.tsx
@@ -12,9 +12,9 @@ import Link from "next/link"
 import { toast } from "sonner"
 import FormField from "./FormField"
 import { useRouter } from "next/navigation"
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "@/firebase/client"
-import { signUp } from "@/lib/actions/auth.action"
+import { signIn, signUp } from "@/lib/actions/auth.action"
 
 const authFormSchema = (type: FormType) => {
    
@@ -65,6 +65,21 @@ const AuthForm = ({type}: {type: FormType}) => {
             }
             else {
                 // console.log("Sign In", values)
+
+                const { email, password } = values;
+                
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+                const idToken = await userCredential.user.getIdToken();
+
+                if(!idToken) {
+                    toast.error('Sign in failed');
+                    return; 
+                }
+                await signIn({
+                    email, idToken
+                })
+                
                 toast.success("Sign in successfully")
                 router.push('/')
             }
